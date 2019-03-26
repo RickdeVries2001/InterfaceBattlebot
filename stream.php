087837973c932fee168be611397569f711c88a46
SylvanLoves <lovessylvan98@gmail.com>
2019-03-26 13:54:34 +0100

    Create stream.php

---
+++ stream.php
@@ -0,0 +1,14 @@
+<?php
+	include "views/header.php";
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-xl">
+			<IFRAME SRC="http://foscam.serverict.nl/videostream.cgi" WIDTH="640px" HEIGHT="480px" FRAMEBORDER="0"> </IFRAME>
+		</div>
+	</div>
+</div>	
+
+<?php
+	include "views/footer.php";
+?>
