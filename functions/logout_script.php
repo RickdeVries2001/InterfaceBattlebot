66d79c4fd25ef3ee0b6b6d5eae4a0dfbc62cb257
Stefan <stefan.schepers@student.nhlstenden.com>
2019-04-01 12:27:42 +0200

    Login script
    Login script werkt met cookies. Uitloggen toegevoegd aan navbar.

---
+++ functions/logout_script.php
@@ -0,0 +1,4 @@
+<?php
+		setcookie("dikkedoei", "", time()-3600);
+		echo "<a href='../index.php'>Terug naar homepagina</a>";
+?>
--- views/notlogbody.php
+++ views/notlogbody.php
@@ -1,29 +1,27 @@
 <div class="container mt-5 contain justify-content-center align-items-center">
-	<form class="mx-auto col-8 loginf mt-5" action="login.php" method="post">
+	<form class="mx-auto col-8 loginf mt-5" action="functions/login_script.php" method="post">
 		<h2 class="white_text ">Please, login into your account</h2>
 		<div class="row mt-4">
 			<div class="col-12">
-				<div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+				<div class="form-group">
 				</div>
 			</div>
 		</div>
 
 		<div class='row'>
-			<div class='input-field col-12 <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>'>
-				<input type="text" id="username" class="form-control mb-2" placeholder="Username" required autofocus>
-				<span class="help-block"><?php echo $username_err; ?></span>
+			<div class='input-field col-12'>
+				<input type="text" name="username" class="form-control mb-2" placeholder="Username" required autofocus>
 			</div>
 		</div>
 		<div class='row mb-3'>
-			<div class='input-field col-12 <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>'>
-				<input type="password" id="password" class="form-control mb-2" placeholder="Password" required autofocus>
-				<span class="help-block"><?php echo $password_err; ?></span>
+			<div class='input-field col-12'>
+				<input type="password" name="password" class="form-control mb-2" placeholder="Password" required autofocus>
 			</div>
 		</div>
 		<center>
 			<div class='row'>
 				<div class="col-12">
-					<button type='submit' name='btn_login' class='col btn btn-primary'>Login</button>
+					<button type='submit' name='submit' class='col btn btn-primary'>Login</button>
 				</div>
 			</div>
 		</center>
