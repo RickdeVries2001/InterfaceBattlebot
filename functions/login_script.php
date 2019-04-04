fdfa381aaebdaada8fc9628c2525a9fe0cc25125
Casper <cms.vandenberg@gmail.com>
2019-04-04 17:12:44 +0200

    login with cookies

--- functions/login_script.php
+++ functions/login_script.php
@@ -6,19 +6,19 @@
   if(isset($_POST['submit'])){
   	$username = ($_POST['username']);
   	$password = ($_POST['password']);
-  	$SQLstring = "SELECT pass, username, btname FROM users WHERE username = ?";
+  	$SQLstring = "SELECT ID, pass, username, btname FROM users WHERE username = ?";
   		if ($stmt = mysqli_prepare($conn, $SQLstring)) {
   			mysqli_stmt_bind_param($stmt, "s", $username);
   			mysqli_stmt_execute($stmt);
-  			mysqli_stmt_bind_result($stmt, $pw, $unamedb, $robotname);
+  			mysqli_stmt_bind_result($stmt, $id, $pw, $unamedb, $robotname);
   			mysqli_stmt_store_result($stmt);
   			if (mysqli_stmt_num_rows($stmt) > 0) {
   				mysqli_stmt_fetch($stmt);
   				if($password === $pw) {
-  					$cookie_value = $unamedb + $robotname;
-  					setcookie("dikkedoei", $cookie_value);
+					$expire = time()+60*60*24*30;
+  					setcookie("id", $id ,$expire);
   					echo "<p>Je bent ingelogd!</p>";
-  					header('Location: ../score.php');
+  					//header('Location: ../score.php');
   				}
   				else { echo 'Verkeerd wachtwoord!';}
   			}
